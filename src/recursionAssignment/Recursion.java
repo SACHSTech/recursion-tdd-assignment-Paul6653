fad6ed69db433200724b44fe13e4caa4b038fddd
Paul Tran <>
2021-05-21 04:26:58 +0000

    problem set 1 passed test2

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -3,10 +3,10 @@ package recursionAssignment;
 public class Recursion{
 
   public static int count7(int n) {
-    if (n == 7) {
+    if (n % 10 == 7) {
       return 1;
     }
-    
+
     return -1;
   }
 
