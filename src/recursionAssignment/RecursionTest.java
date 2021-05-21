d50666ac29f0e7cf7b4c85532c662185c0ca65c7
Paul Tran <>
2021-05-21 04:25:41 +0000

    problem set 1 created test2

--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -6,12 +6,15 @@ import recursionAssignment.Recursion;
 
 public class RecursionTest{
 
-  /**
-    * An initial test
-    */
+  //Problem set 1 tests
   @Test
   public void Test1() {
     assertEquals(1, Recursion.count7(7));
+  }
+
+  @Test
+  public void Test2() {
+    assertEquals(1, Recursion.count7(27));
   }
 
 }
