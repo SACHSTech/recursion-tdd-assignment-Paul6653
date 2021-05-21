88f40ddefe8143da078c0b2838a2864361c36808
Paul Tran <>
2021-05-21 20:32:34 +0000

    added documentation

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -4,7 +4,7 @@ public class Recursion{
   
   /**
     * Given a non-negative integer, return the number of occurrences of the digit 7
-    * @param integer1 non-negative integer
+    * @param n non-negative integer
     * @return the count of occurrences of the digit 7
     * @author P. Tran
     */
@@ -22,7 +22,7 @@ public class Recursion{
 
   /**
     * Given a string, return a new string where all lowercase 'x' chars have been moved to the end of the string
-    * @param string1 random string
+    * @param str random string
     * @return new string with 'x' chars moved to the end
     * @author P. Tran
     */
@@ -41,6 +41,13 @@ public class Recursion{
     return str.substring(0,1) + endX(str.substring(1));
   }
 
+  /**
+    * Given an integer array, return true if any of the values are followed by a value 10 times larger
+    * @param1 intNums integer array
+    * @param2 index integer to check array
+    * @return true if any of the values are followed by a value 10 times larger, otherwise false
+    * @author P. Tran
+    */
   public static boolean array220(int[] intNums, int index) {
     //base case
     if (index == intNums.length - 1) {
@@ -48,7 +55,7 @@ public class Recursion{
     }
 
     /**
-      *if the value followed after index is 10 times bigger, return true and end, 
+      *if the value followed after index is 10 times larger, return true and end, 
       *else, check the next index
       */
     if (intNums[index] * 10 == intNums[index + 1]) {
