ae94b56cc02234e39f8992026d2b2c123f5b53fe
Paul Tran <>
2021-05-21 04:47:55 +0000

    added documentation

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -1,6 +1,12 @@
 package recursionAssignment;
 
 public class Recursion{
+  /**
+    * Given a non-negative integer, return the number of occurrences of the digit 7
+    * @param integer1 non-negative integer
+    * @return the count of occurrences of the digit 7
+    * @author P. Tran
+    */
 
   public static int count7(int n) {
     if (n == 0) {
